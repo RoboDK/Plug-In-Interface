309420010f1650854f158949321056255dee7fa3
RoboDK <anubiola@robodk.com>
2021-09-29 00:37:20 +0200

    RoboDK 5.3.0 officially released with the new plugin interface

--- robodk_interface/iapprobodk.h
+++ robodk_interface/iapprobodk.h
@@ -97,9 +97,9 @@
 * \subsection LinkInstall Installation Requirements
 * Requirements to make RoboDK Plug-Ins work:
 * - It is required to install RoboDK (v3.5.4 or later): https://robodk.com/download
-* - Make sure you properly run your plugin using the following version of Qt:
-*   - Qt version 5.11.2 on Windows. Make sure to use Qt built for MSVC2017.
-*   - Qt version 5.12 on Mac.
+* - Make sure you compile your plugin using the correct version of Qt and compiler:
+*   - Qt version 5.15.6 on Windows (MSVC2019).
+*   - Qt version 5.15.2 on Mac (clang 64 bit).
 *   - Qt version 5.12 on Linux.
 * - It is possible to run in \ref LinkDebug debug mode on Windows.
 *
