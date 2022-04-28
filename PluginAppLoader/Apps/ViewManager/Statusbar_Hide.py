d26720cff7e7848fbca9adfa6a72bade6b67054c
RoboDK <anubiola@robodk.com>
2022-04-28 18:50:04 +0200

    Added option to show/hide the status bar

---
+++ PluginAppLoader/Apps/ViewManager/Statusbar_Hide.py
@@ -0,0 +1,10 @@
+# Show the status bar of RoboDK
+from robodk import robolink
+
+RDK = robolink.Robolink()
+
+# FLAG_ROBODK_REFERENCES_VISIBLE = 16384      # Make the reference frames visible
+FLAG_ROBODK_STATUSBAR_VISIBLE = 2*robolink.FLAG_ROBODK_REFERENCES_VISIBLE
+flagsrdk = robolink.FLAG_ROBODK_ALL & ~FLAG_ROBODK_STATUSBAR_VISIBLE
+
+RDK.setFlagsRoboDK(flagsrdk) # Detached and hidden
